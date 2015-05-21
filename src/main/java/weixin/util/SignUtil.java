d868ffb0ff78f4109d91bbe423fc0ad0daeabd87
lichencai <793385991@qq.com>
2015-05-21 08:55:09 +0800

    20150521

--- src/main/java/weixin/util/SignUtil.java
+++ src/main/java/weixin/util/SignUtil.java
@@ -18,7 +18,7 @@ public class SignUtil {
      * @return 
      */  
     public static boolean checkSignature(String signature, String timestamp, String nonce) {  
-        String[] arr = new String[] { token, timestamp, nonce };  
+        String[] arr = new String[] { token, timestamp, nonce }; 	 
         // 将token、timestamp、nonce三个参数进行字典序排序  
         Arrays.sort(arr);  
         StringBuilder content = new StringBuilder();  
